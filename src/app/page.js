3b2c7f615a2a6c5924f79c2fad76883b9175b68d
JasonTai <r041291867@gmail.com>
2024-10-29 00:19:23 +0800

    調整為萬聖節活動用菜單

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import CocktailItem from "@/components/cocktailItem";
 import Loading from "@/components/loading";
 import HiddenPage from "@/components/hiddenPage";
-import { cocktailMenu } from "@/data/cocktails";
+import { cocktailMenu } from "@/data/cocktailsHalloween";
 import Popup from "@/components/popup";
 import Reciepe from "@/components/reciepe";
 import { getReciepe } from '@/data/reciepe.js'
---
+++ src/data/cocktailsHalloween.js
@@ -0,0 +1,98 @@
+export const cocktailMenu = [
+  {
+    category: "Signature",
+    categoryCh: "特調",
+    cocktails: [
+      {
+        nameCht: "牛奶糖",
+        nameEng: "Fudge",
+        hasImage: true,
+        src: "/images/cocktails/alexandar.jpg",
+        imgPosition: "left",
+      },
+      {
+        nameCht: "檸檬塔",
+        nameEng: "Lemon Tart",
+        hasImage: true,
+        src: "/images/cocktails/ramosGinFizz.jpg",
+        imgPosition: "left",
+      },
+      {
+        nameCht: "百香伯爵",
+        nameEng: "Passion Earl",
+      },
+    ],
+  },
+  {
+    category: "Sour",
+    categoryCh: "清新酸爽",
+    cocktails: [
+      {
+        nameCht: "莫斯科驢子",
+        nameEng: "Moscow Mule",
+      },
+      {
+        nameCht: "琴蕾",
+        nameEng: "Gimlet",
+      },
+      {
+        nameCht: "黛綺莉",
+        nameEng: "Daiquiri",
+      },
+      {
+        nameCht: "淘金熱",
+        nameEng: "Gold Rush",
+        hasImage: true,
+        src: "/images/cocktails/goldRush.jpg",
+        imgPosition: "right",
+      },
+      {
+        nameCht: "蜜蜂之膝",
+        nameEng: "Bee's Knees",
+        hasImage: true,
+        src: "/images/cocktails/beesKnees.jpg",
+        imgPosition: "right",
+      },
+    ],
+  },
+  {
+    category: "Classics",
+    categoryCh: "懷舊經典",
+    cocktails: [
+      {
+        nameCht: "白色佳人",
+        nameEng: "White Lady",
+      },
+      {
+        nameCht: "邁泰",
+        nameEng: "Mai-Tai",
+      },
+      {
+        nameCht: "往日情懷",
+        nameEng: "Old Fashioned",
+      },
+    ],
+  },
+  {
+    category: "Just Relax",
+    categoryCh: "輕鬆喝",
+    cocktails: [
+      {
+        nameCht: "嗨波魯",
+        nameEng: "Highball",
+      },
+      {
+        nameCht: "琴費士",
+        nameEng: "Gin Fizz",
+      },
+      {
+        nameCht: "琴通寧",
+        nameEng: "Gin & Tonic",
+      },
+      {
+        nameCht: "會館費士",
+        nameEng: "Kaikan Fizz",
+      },
+    ],
+  },
+];
